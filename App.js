cbb2302697eb7416b78d84b2eea3a1136b513c18
Ananya Doshi <doshiananya2002@gmail.com>
2023-01-18 23:52:07 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video style={styles.cont1}
+        ref={video}
+        
+        source={{
+          uri: 'https://firebasestorage.googleapis.com/v0/b/fir-video-62114.appspot.com/o/file_example_MP4_480_1_5MG.mp4?alt=media&token=78373b25-b008-4e92-8b8a-cec950e7e11f',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View>
+        <Button 
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{ flex: 1, justifyContent: "center"},
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
+  cont1:{
+    width:'100%',
+    flex:1,
+  }
 });
